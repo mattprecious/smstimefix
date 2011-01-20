a6af765f5303e2590a5d0d047da51da04a6b749e
Matthew Precious <matt@mattprecious.com>
2011-01-20 15:15:03 +0800

    Formatting Cleaned up the code formatting. Most importantly, changed tabs to 4x spaces.

--- src/com/mattprecious/smsfix/Help.java
+++ src/com/mattprecious/smsfix/Help.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Matthew Precious
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,15 +22,15 @@ import android.os.Bundle;
 /**
  * 
  * @author Matthew Precious
- *
+ * 
  */
 public class Help extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.help);
-		// TODO: update the help page to reflect the new UI
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+        // TODO: update the help page to reflect the new UI
+    }
 
 }
